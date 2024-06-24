0592c7cbca30916f88e38bbf1d326f4e0f852f67
ngvior18 <ngvior@gmail.com>
2024-06-24 20:33:51 -0300

    refinamiento endpoints admin

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ import morgan from "morgan";
 import usuariosRoutes from "./routes/Usuarios.js";
 import categoriasRouter from "./routes/categorias.js";
 import productosRouter from "./routes/productos.js";
-import registroRouter from './routes/registroUsuarios.js';
+import registroRouter from "./routes/registroUsuarios.js";
+import adminRouter from "./routes/Admin.js";
 import cors from "cors";
 
 const PORT = process.env.PORT || 3000;
@@ -15,8 +16,9 @@ app.use(morgan("dev"));
 app.use("/api/usuarios", usuariosRoutes);
 app.use("/api/categorias", categoriasRouter);
 app.use("/api/productos", productosRouter);
-app.use('/api/', registroRouter);
+app.use("/api/admin", adminRouter);
+app.use("/api/", registroRouter);
 
 app.listen(PORT, () => {
-    console.log(`Server running in http://localhost:${PORT}`);
+  console.log(`Server running in http://localhost:${PORT}`);
 });
--- data/Admin.js
+++ data/Admin.js
@@ -1,58 +1,56 @@
 import "dotenv/config";
 import { ObjectId } from "mongodb";
 import getConnection from "./conn.js";
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 const DATABASE = process.env.DATABASE;
 const USUARIOS = process.env.USUARIOS;
-const ERROR_INSERTAR = "Error al insertar la categoría: ";
-const ERROR_OBTENER = "Error al obtener las categorías";
-const ERROR_OBTENER_ID = (id) => `Error al obtener categoría (id ${id})`;
 
-//funcion para POST que crea una categoria nueva, a partir de un objeto recibido por parametro.
-//categoria debe ser { nombre: ... }
+
 export async function loginAdmin(userEmail, userPassword) {
-    try{
-        const usuario = {email: userEmail, password: userPassword}
-        const verificarUsuario = await getUsuario(usuario);
+  try {
+    const usuario = { email: userEmail, password: userPassword };
+    const verificarUsuario = await getUsuario(usuario);
 
-        if (!usuarioAdmin(verificarUsuario)) {
-        return new Error("El usuario no es admin.");
-        } 
-        const token = await generarAuthToken(verificarUsuario);
-        return token;
-    }catch(error){
-        return new Error(error.message);
+    if (!usuarioAdmin(verificarUsuario)) {
+      return new Error("El usuario no es admin.");
     }
+    const token = await generarAuthToken(verificarUsuario);
+    return token;
+  } catch (error) {
+    return new Error(error.message);
+  }
 }
 
 //valida que el objeto categoria tenga .nombre
 function usuarioAdmin(usuario) {
-    return usuario.admin ? true : false;
+  return usuario.admin ? true : false;
+}
+
+async function getUsuario(usuario) {
+  const conndb = await getConnection();
+  const user = await conndb
+    .db(DATABASE)
+    .COLLECTION(USUARIOS)
+    .findOne({ email: usuario.email });
+
+  if (!user) {
+    throw new Error("Usuario o contraseña incorrecta.");
   }
 
+  const isMatch = await bcryptjs.compare(usuario.password, user.password);
+  if (!isMatch) {
+    throw new Error("Usuario o contraseña incorrecta.");
+  }
 
-async function getUsuario(usuario){
-    const conndb = await getConnection();
-    const user = await conndb.db(DATABASE).COLLECTION(USUARIOS).findOne({email: usuario.email});
-    
-    if(!user){
-        throw new Error("Usuario o contraseña incorrecta.");
-    }
-
-    const isMatch = await bcrypt.compare(usuario.password, user.password)
-    if(!isMatch){
-        throw new Error("Usuario o contraseña incorrecta.");
-    }
-
-    return usuario;
+  return usuario;
 }
 
 export async function generarAuthToken(usuario) {
-    const token = await jwt.sign(
-      { _id: usuario._id, email: usuario.email },
-      process.env.CLAVE_SECRETA,
-      { expiresIn: "2h" }
-    );
-    return token;
-};
+  const token = await jwt.sign(
+    { _id: usuario._id, email: usuario.email },
+    process.env.CLAVE_SECRETA,
+    { expiresIn: "2h" }
+  );
+  return token;
+}
